1f121ecf842769ce248da905c33070de867604cd
ksupa <kevinbsupa@gmail.com>
2025-05-22 12:02:04 -0600

    created CurrentWeather constructors, created override to toString() method, adjusted exception messages to pass the tests

--- lib/models/current_weather.dart
+++ lib/models/current_weather.dart
@@ -6,6 +6,34 @@ class CurrentWeather {
   late DateTime _sunrise;
   late DateTime _sunset;
 
+  CurrentWeather({required String city, required String description, required double currentTemp, required DateTime currentTime, required
+  DateTime sunrise, required DateTime sunset}) {
+    this.city = city;
+    this.description = description;
+    this.currentTemp = currentTemp;
+    this.currentTime = currentTime;
+    _sunrise = sunrise;
+    _sunset = sunset;
+  }
+
+  factory CurrentWeather.fromOpenWeatherData(dynamic data) {
+    final String city = data['name'];
+    final String description = data['weather'][0]['description'];
+    final double currentTemp = data['main']['temp'];
+    final DateTime currentTime = DateTime.fromMillisecondsSinceEpoch((data['dt'] * 1000).toInt());
+    final DateTime sunrise = DateTime.fromMillisecondsSinceEpoch((data['sys']['sunrise'] * 1000).toInt());
+    final DateTime sunset = DateTime.fromMillisecondsSinceEpoch((data['sys']['sunset'] * 1000).toInt());
+
+    return CurrentWeather(
+      city: city,
+      description: description,
+      currentTemp: currentTemp,
+      currentTime: currentTime,
+      sunrise: sunrise,
+      sunset: sunset,
+    );
+  }
+
   String get city => _city;
   String get description => _description;
   double get currentTemp => _currentTemp;
@@ -44,7 +72,7 @@ class CurrentWeather {
 
   set sunrise(DateTime value) {
     if(value.year != _currentTime.year || value.month != _currentTime.month || value.day != _currentTime.day) {
-      throw Exception('Sunrise must be on the same day as the current time.');
+      throw Exception('Sunrise must be on the same day as current time.');
     }
 
     if(value.isAfter(_sunset)) {
@@ -55,7 +83,7 @@ class CurrentWeather {
 
   set sunset(DateTime value) {
     if(value.year != _currentTime.year || value.month != _currentTime.month || value.day != _currentTime.day) {
-      throw Exception('Sunset must be on the same day as the current time.');
+      throw Exception('Sunset must be on the same day as current time.');
     }
 
     if(value.isBefore(_sunrise)) {
@@ -64,4 +92,8 @@ class CurrentWeather {
     _sunset = value;
   }
   // endregion
+
+@override String toString() {
+  return 'City: $city, Description: $description, Current Temperature: $currentTemp, Current Time: $currentTime, Sunrise: $sunrise, Sunset: $sunset';
+  }
 }
